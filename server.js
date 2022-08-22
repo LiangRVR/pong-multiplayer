148866850209ed5a29cdea0fdc273e8837f9a492
LiangRVR <liangvillarrubia95@gmail.com>
2022-08-22 14:51:31 -0400

    feat: connect players and set a referee

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const server = require("node:http").createServer();
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://127.0.0.1:3001",
+    methods: ["GET", "POST"],
+  },
+});
 
 const PORT = 3000;
 
 server.listen(PORT);
 console.log(`Server Listening in port ${PORT}`);
 
-io.on('connection', (socket)=>{
-    console.log('user connected')
-})
+let readyPlayerCount = 0;
+
+io.on("connection", (socket) => {
+  socket.on("ready", () => {
+    console.log("Player ready ", socket.id);
+    readyPlayerCount++;
+    if (readyPlayerCount === 2) {
+      console.log(readyPlayerCount);
+      io.emit("startGame", socket.id);
+    }
+  });
+});
